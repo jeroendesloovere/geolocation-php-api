06414152789fe136ed3ad1e26ceb3f0258fb37e1
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-03-19 09:50:25 +0100

    PSR-2-code-styling applied

--- src/Geolocation/Geolocation.php
+++ src/Geolocation/Geolocation.php
@@ -9,134 +9,134 @@
  */
 class Geolocation
 {
-	// API URL
-	const API_URL = 'http://maps.googleapis.com/maps/api/geocode/json';
+    // API URL
+    const API_URL = 'http://maps.googleapis.com/maps/api/geocode/json';
 
-	/**
-	 * Do call
-	 *
-	 * @param array $parameters
-	 * @return object
-	 */
-	protected static function doCall($parameters = array())
-	{
-		// check if curl is available
-		if(!function_exists('curl_init'))
-		{
-			throw new GeolocationException('This method requires cURL (http://php.net/curl), it seems like the extension isn\'t installed.');
-		}
+    /**
+     * Do call
+     *
+     * @return object
+     * @param array $parameters
+     */
+    protected static function doCall($parameters = array())
+    {
+        // check if curl is available
+        if (!function_exists('curl_init')) {
+            // throw error
+            throw new GeolocationException('This method requires cURL (http://php.net/curl), it seems like the extension isn\'t installed.');
+        }
 
-		// define url
-		$url = self::API_URL . '?';
+        // define url
+        $url = self::API_URL . '?';
 
-		// add every parameter to the url
-		foreach($parameters as $key => $value) $url .= $key . '=' . urlencode($value) . '&';
+        // add every parameter to the url
+        foreach ($parameters as $key => $value) $url .= $key . '=' . urlencode($value) . '&';
 
-		// trim last &
-		$url = trim($url, '&');
+        // trim last &
+        $url = trim($url, '&');
 
-		// init curl
-		$curl = curl_init();
+        // init curl
+        $curl = curl_init();
 
-		// set options
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-		if(ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        // set options
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 
-		// execute
-		$response = curl_exec($curl);
+        // execute
+        $response = curl_exec($curl);
 
-		// fetch errors
-		$errorNumber = curl_errno($curl);
-		$errorMessage = curl_error($curl);
+        // fetch errors
+        $errorNumber = curl_errno($curl);
+        $errorMessage = curl_error($curl);
 
-		// close curl
-		curl_close($curl);
+        // close curl
+        curl_close($curl);
 
-		// we have errors
-		if($errorNumber != '') throw new GeolocationException($errorMessage);
+        // we have errors
+        if ($errorNumber != '') throw new GeolocationException($errorMessage);
 
-		// redefine response as json decoded
-		$response = json_decode($response);
+        // redefine response as json decoded
+        $response = json_decode($response);
 
-		// return the content
-		return $response->results;
-	}
-	
-	/**
-	 * Get address using latitude/longitude
-	 *
-	 * @param float $latitude
-	 * @param float $longitude
-	 * @return array(label, street, streetNumber, city, cityLocal, zip, country, countryLabel)
-	 */
-	public static function getAddress($latitude, $longitude)
-	{
-		// define result
-		$results = self::doCall(array(
-			'latlng' => $latitude . ',' . $longitude,
-			'sensor' => 'false'
-		));
+        // return the content
+        return $response->results;
+    }
+    
+    /**
+     * Get address using latitude/longitude
+     *
+     * @return array(label, street, streetNumber, city, cityLocal, zip, country, countryLabel)
+     * @param float $latitude
+     * @param float $longitude
+     */
+    public static function getAddress($latitude, $longitude)
+    {
+        // define result
+        $results = self::doCall(array(
+            'latlng' => $latitude . ',' . $longitude,
+            'sensor' => 'false'
+        ));
 
-		// return address
-		return array(
-			'label' => (string) $results[0]->formatted_address,
-			'street' => (string) $results[0]->address_components[1]->short_name,
-			'streetNumber' => (string) $results[0]->address_components[0]->short_name,
-			'city' => (string) $results[0]->address_components[3]->short_name,
-			'cityLocal' => (string) $results[0]->address_components[2]->short_name,
-			'zip' => (string) $results[0]->address_components[7]->short_name,
-			'country' => (string) $results[0]->address_components[6]->short_name,
-			'countryLabel' => (string) $results[0]->address_components[6]->long_name
-		);	
-	}
+        // return address
+        return array(
+            'label' => (string) $results[0]->formatted_address,
+            'street' => (string) $results[0]->address_components[1]->short_name,
+            'streetNumber' => (string) $results[0]->address_components[0]->short_name,
+            'city' => (string) $results[0]->address_components[3]->short_name,
+            'cityLocal' => (string) $results[0]->address_components[2]->short_name,
+            'zip' => (string) $results[0]->address_components[7]->short_name,
+            'country' => (string) $results[0]->address_components[6]->short_name,
+            'countryLabel' => (string) $results[0]->address_components[6]->long_name
+        );    
+    }
 
-	/**
-	 * Get coordinates latitude/longitude
-	 *
-	 * @param string $street[optional]
-	 * @param string $streetNumber[optional]
-	 * @param string $city[optional]
-	 * @param string $zip[optional]
-	 * @param string $country[optional]
-	 * @return array The latitude/longitude coordinates
-	 */
-	public static function getCoordinates($street = null, $streetNumber = null, $city = null, $zip = null, $country = null)
-	{
-		// init item
-		$item = array();
+    /**
+     * Get coordinates latitude/longitude
+     *
+     * @return array The latitude/longitude coordinates
+     * @param string $street[optional]
+     * @param string $streetNumber[optional]
+     * @param string $city[optional]
+     * @param string $zip[optional]
+     * @param string $country[optional]
+     */
+    public static function getCoordinates($street = null, $streetNumber = null, $city = null, $zip = null, $country = null)
+    {
+        // init item
+        $item = array();
 
-		// add street
-		if(!empty($street)) $item[] = $street;
+        // add street
+        if (!empty($street)) $item[] = $street;
 
-		// add street number
-		if(!empty($streetNumber)) $item[] = $streetNumber;
+        // add street number
+        if (!empty($streetNumber)) $item[] = $streetNumber;
 
-		// add city
-		if(!empty($city)) $item[] = $city;
+        // add city
+        if (!empty($city)) $item[] = $city;
 
-		// add zip
-		if(!empty($zip)) $item[] = $zip;
+        // add zip
+        if (!empty($zip)) $item[] = $zip;
 
-		// add country
-		if(!empty($country)) $item[] = $country;
+        // add country
+        if (!empty($country)) $item[] = $country;
 
-		// define value
-		$address = implode(' ', $item);
+        // define value
+        $address = implode(' ', $item);
 
-		// define result
-		$results = self::doCall(array(
-			'address' => $address,
-			'sensor' => 'false'
-		));
+        // define result
+        $results = self::doCall(array(
+            'address' => $address,
+            'sensor' => 'false'
+        ));
 
-		// return coordinates latitude/longitude
-		return array(
-			'latitude' => (float) $results[0]->geometry->location->lat,
-			'longitude' => (float) $results[0]->geometry->location->lng
-		);
-	}
+        // return coordinates latitude/longitude
+        return array(
+            'latitude' => (float) $results[0]->geometry->location->lat,
+            'longitude' => (float) $results[0]->geometry->location->lng
+        );
+    }
 }
 
 
